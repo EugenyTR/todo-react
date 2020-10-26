e88a233b8e09bbfa5f30bcd6ab640577cd90646b
Eugeny Semenov <et@takeseoeasy.com>
2020-10-26 18:04:35 +0700

    Updates

---
+++ precache-manifest.92fe265ef0c0291ed873c69f56728e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244eedf7e21bc44e70662150252b7bf5",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "63b97a6e8fab54b83aee",
+    "url": "/todo-react/static/css/main.173cfa3b.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b97a6e8fab54b83aee",
+    "url": "/todo-react/static/js/main.dd58f8c3.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
